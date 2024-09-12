27942dea3b1b19d8a808132f844f1dd75518e395
Taylor Hart <82608807+thart003@users.noreply.github.com>
2024-09-12 10:15:24 -0400

    Update schedules.py

--- schedules.py
+++ schedules.py
@@ -1,1 +1,15 @@
+from .assets import mysql_to_clickhouse_job
+from dagster import schedule, DefaultScheduleStatus, RunRequest
 
+@schedule(
+    job=mysql_to_clickhouse_job,
+    cron_schedule="0 8 * * *",
+    name="mysql_to_clickhouse_schedule",
+    execution_timezone="America/New_York",
+    default_status=DefaultScheduleStatus.STOPPED
+)
+
+def mysql_to_clickhouse_schedule():
+    return RunRequest(
+        job_name="mysql_to_clickhouse_job",
+    )
