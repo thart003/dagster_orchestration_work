e4d1aed29f2053eba58ba0d179d584102b8a7309
Taylor Hart <82608807+thart003@users.noreply.github.com>
2024-09-12 10:16:27 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ads",
+    packages=find_packages(exclude=["ads_tests"]),
+    install_requires=[
+        "dagster",
+        "dagster-cloud"
+    ],
+    extras_require={"dev": ["dagster-webserver", "pytest"]},
+)
 
